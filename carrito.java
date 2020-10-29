1cf899dd54fe3b917c7a0e01bfd12da1ef6a787e
Shuz3t <73603328+Shuz3t@users.noreply.github.com>
2020-10-28 20:44:42 -0700

    Update carrito.java

--- carrito.java
+++ carrito.java
@@ -10,6 +10,35 @@ public class carrito
         this.total = total;
         this.productos = productos;
     }
+	public String getNombre()
+    {
+		return nombre;
+	}
+
+	public void setNombre(String nombre)
+    {
+		this.nombre = nombre;
+	}
+
+	public float getTotal() 
+    {
+		return total;
+	}
+
+	public void setTotal(float total) 
+    {
+		this.total = total;
+	}
+
+	public ListaDoble<Producto> getProductos() 
+    {
+	   return productos;
+	}
+
+	public void setProductos(ListaDoble<Producto> productos)
+    {
+	   this.productos = productos;
+	}
 
     
     
